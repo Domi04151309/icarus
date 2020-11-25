9f92355a60cda9e928be0203189e8fcdf4f8d6d9
Domi04151309 <>
2020-11-25 17:28:56 +0100

    Add content to week page

--- js/pages/week.js
+++ js/pages/week.js
@@ -12,15 +12,48 @@ export default {
       <div class="card mb-16-p-16">\
         <h2>General Progress <span class="material-icons-round c-icon">table_view</span></h2>\
         <progress max="100" value="65"></progress>\
-        <p>This is your general progress for today. It combines your progress of the sections below.</p>\
+        <p>This is your general progress for this week. It combines your progress of the sections below.</p>\
       </div>\
       <div class="card mb-16-p-16">\
-        <h2>Daily View <span class="material-icons-round c-icon">view_week</span></h2>\
-        <p>TODO: Add vertical bars</p>\
+        <h2>Daily View <span class="material-icons-round c-icon">pending_actions</span></h2>\
+        <div class="flex vertical-container">\
+          <progress class="vertical" max="100" value="33"></progress>\
+          <progress class="vertical" max="100" value="33"></progress>\
+          <progress class="vertical" max="100" value="33"></progress>\
+          <progress class="vertical" max="100" value="33"></progress>\
+          <progress class="vertical" max="100" value="33"></progress>\
+          <progress class="vertical" max="100" value="33"></progress>\
+          <progress class="vertical" max="100" value="33"></progress>\
+        </div>\
+        <div class="flex space">\
+          <p>M</p>\
+          <p>T</p>\
+          <p>W</p>\
+          <p>T</p>\
+          <p>F</p>\
+          <p>S</p>\
+          <p>S</p>\
+        </div>\
       </div>\
       <div class="card mb-16-p-16">\
-        <h2>Compared to Last Week <span class="material-icons-round c-icon">compare_arrows</span></h2>\
-        <p>TODO: Add improvement table</p>\
+        <h2>Compared to Last Week <span class="material-icons-round c-icon">sync_alt</span></h2>\
+        <div class="grid-3">\
+          <p class="progress-text mt-8 mb-16">Last Week</p>\
+          <span></span>\
+          <p class="progress-text mt-8 mb-16">This Week</p>\
+          <progress class="reverse mb-16" max="100" value="33"></progress>\
+          <p class="progress-text">Water</p>\
+          <progress class="mb-16" max="100" value="33"></progress>\
+          <progress class="reverse mb-16 indigo" max="100" value="33"></progress>\
+          <p class="progress-text">Food</p>\
+          <progress class="mb-16 indigo" max="100" value="33"></progress>\
+          <progress class="reverse mb-16 red" max="100" value="33"></progress>\
+          <p class="progress-text">Exercices</p>\
+          <progress class="mb-16 red" max="100" value="33"></progress>\
+          <progress class="reverse mb-16 deep-orange" max="100" value="33"></progress>\
+          <p class="progress-text">Sleep</p>\
+          <progress class="mb-16 deep-orange" max="100" value="33"></progress>\
+        </div>\
       </div>\
     </page>',
   components: {
