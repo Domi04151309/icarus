bbfdfa4b6fc12684d081d2aee407504028b6df0f
Domi04151309 <>
2020-12-05 22:09:41 +0100

    Improve food layout

--- js/pages/add-food.js
+++ js/pages/add-food.js
@@ -34,6 +34,10 @@ export default {
       <span class="material-icons-round accent img">{{ icon }}</span>\
       <span>{{ foodTitle }}</span>\
     </h2>\
+    <p class="mb-16">\
+      Below you can see the values of this item.\
+      Use the button in the bottom right corner to complete the action.\
+    </p>\
     <div ref="title">\
       <label for="foodTitle">Title</label>\
       <input id="foodTitle" v-model="foodTitle" class="simple-input mb-16" type="text"></input>\
@@ -47,14 +51,16 @@ export default {
         <label for="carbs">Carbs</label>\
         <input id="carbs" ref="carbs" class="simple-input" type="number" value="0"></input>\
       </div>\
-      <div>\
+      <div class="mb-16">\
         <label for="proteins">Proteins</label>\
         <input id="proteins" ref="proteins" class="simple-input" type="number" value="0"></input>\
       </div>\
-      <div>\
+      <div class="mb-16">\
         <label for="fat">Fat</label>\
         <input id="fat" ref="fat" class="simple-input" type="number" value="0"></input>\
       </div>\
+      <button ref="edit" type="button" v-on:click="">Edit</button>\
+      <button ref="delete" type="button" v-on:click="">Delete</button>\
     </div>\
     <div ref="fab" class="material-icons-round fab hidden">done</div>\
   </page>',
@@ -62,6 +68,9 @@ export default {
     if (this.$route.query.item != undefined) {
       this.foodTitle = this.$route.query.item
       this.$refs.title.classList.add('hidden')
+    } else {
+      this.$refs.edit.classList.add('hidden')
+      this.$refs.delete.classList.add('hidden')
     }
     setTimeout(() => { this.$refs.fab.classList.remove('hidden') }, 500)
   },
