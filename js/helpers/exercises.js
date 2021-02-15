2521909997d2b9e0a40c8c088b13f1f4e5e1d249
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-02-15 19:39:01 +0100

    Add exercise list

---
+++ js/helpers/exercises.js
@@ -0,0 +1,82 @@
+export default [
+	{
+		name: 'Push-Ups',
+		variations: [
+			{
+				name: 'Standart',
+				fatLoss: 0,
+				muscleGain: 1,
+				cardio: -1,
+				endurance: 0,
+				arms: 1,
+				shoulders: 1,
+				back: 0,
+				chest: 1,
+				abs: -1,
+				booty: -1,
+				legs: -1
+			},
+			{
+				name: 'Diamond',
+				fatLoss: 0,
+				muscleGain: 1,
+				cardio: -1,
+				endurance: 0,
+				arms: 1,
+				shoulders: 1,
+				back: 0,
+				chest: 1,
+				abs: -1,
+				booty: -1,
+				legs: -1
+			}
+		]
+	},
+	{
+		name: 'Sit-Ups',
+		variations: [
+			{
+				name: 'Standart',
+				fatLoss: 0,
+				muscleGain: 1,
+				cardio: 0,
+				endurance: 0,
+				arms: -1,
+				shoulders: -1,
+				back: -1,
+				chest: -1,
+				abs: 1,
+				booty: -1,
+				legs: -1
+			},
+			{
+				name: 'Feed Up',
+				fatLoss: 0,
+				muscleGain: 1,
+				cardio: 0,
+				endurance: 0,
+				arms: -1,
+				shoulders: -1,
+				back: -1,
+				chest: -1,
+				abs: 1,
+				booty: -1,
+				legs: -1
+			},
+			{
+				name: 'Sideways',
+				fatLoss: 0,
+				muscleGain: 1,
+				cardio: 0,
+				endurance: 0,
+				arms: -1,
+				shoulders: -1,
+				back: 0,
+				chest: -1,
+				abs: -1,
+				booty: 1,
+				legs: 1
+			}
+		]
+	}
+]
--- js/pages/exercise-list.js
+++ js/pages/exercise-list.js
@@ -1,28 +1,28 @@
 import Page from '../components/page.js'
 
+import Exercises from '../helpers/exercises.js'
+
 export default {
   name: 'exercise-list',
   data() {
     return {
       title: 'Exercises',
-      storage: [
-        { title: 'Exercise 1', dummy: 'dummy text' },
-        { title: 'Exercise 2', dummy: 'dummy text' },
-        { title: 'Exercise 3', dummy: 'dummy text' },
-        { title: 'Exercise 4', dummy: 'dummy text' }
-      ]
+      storage: Exercises
     }
   },
   template:
   `<page :title="title" parent="/exercises">
-    <ul class="link-list ignore-page-padding">
-      <li v-for="(item, index) in storage" :key="index">
-        <span>
-          {{ item.title }}<br>
-          <span class="p">{{ item.dummy }}</span>
-        </span>
-      </li>
-    </ul>
+    <div class="card mb-16" v-for="item, index) in storage" :key="index + item.name">
+      <h2 class="p-16 m-0">{{ item.name }}</h2>
+      <ul class="link-list m-0">
+        <li v-for="variation in item.variations" :key="index + variation.name">
+          <span>
+            {{ variation.name }}<br>
+            <span class="p">Recommended</span>
+          </span>
+        </li>
+      </ul>
+    </div>
   </page>`,
   components: {
       Page
