1f219f0a261ae98e2f0bc939b21c92a75700b736
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-03-04 22:04:00 +0100

    Add very experimental scoring

--- js/pages/exercise-list.js
+++ js/pages/exercise-list.js
@@ -1,6 +1,7 @@
 import Page from '../components/page.js'
 
-import Exercises from '../helpers/exercises.js'
+import Exercises from '../data/exercises.js'
+import ExercisesHelper from '../helpers/exercises.js'
 
 export default {
   name: 'exercise-list',
@@ -10,6 +11,9 @@ export default {
       storage: Exercises
     }
   },
+  computed: {
+    ExercisesHelper: () => ExercisesHelper
+  },
   template:
   `<page :title="title" parent="/exercises">
     <div class="card mb-16" v-for="item, index) in storage" :key="index + item.name">
@@ -18,7 +22,7 @@ export default {
         <li v-for="variation in item.variations" :key="index + variation.name">
           <span>
             {{ variation.name }}<br>
-            <span class="p">Loading</span>
+            <span class="p">Score: {{ ExercisesHelper.getScore(variation) }}</span>
           </span>
         </li>
       </ul>
--- js/pages/setup/4-fitness.js
+++ js/pages/setup/4-fitness.js
@@ -2,6 +2,22 @@ import PageNoAppBar from '../../components/page-no-app-bar.js'
 
 export default {
   name: 'setup-fitness',
+  data() {
+    return {
+      fitness: {
+        muscleGain: 50,
+        cardio: 50,
+        endurance: 50,
+        arms: 50,
+        shoulders: 50,
+        back: 50,
+        chest: 50,
+        abs: 50,
+        booty: 50,
+        legs: 50
+      }
+    }
+  },
   template:
   `<div>
     <div class="setup-image info"></div>
@@ -12,25 +28,25 @@ export default {
         Leave this as-is if you want a balanced workout.
       </p>
       <label for="muscle_gain">Muscle gain</label>
-      <input id="muscle_gain" ref="muscle_gain" class="mb-16" type="range" max="100">
+      <input id="muscle_gain" class="mb-16" type="range" max="100" v-model="fitness.muscleGain">
       <label for="cardio">Cardio</label>
-      <input id="cardio" ref="cardio" class="mb-16" type="range" max="100">
+      <input id="cardio" class="mb-16" type="range" max="100" v-model="fitness.cardio">
       <label for="endurance">Endurance</label>
-      <input id="endurance" ref="endurance" class="mb-16" type="range" max="100">
+      <input id="endurance" class="mb-16" type="range" max="100" v-model="fitness.endurance">
       <label for="arms">Arms</label>
-      <input id="arms" ref="arms" class="mb-16" type="range" max="100">
+      <input id="arms" class="mb-16" type="range" max="100" v-model="fitness.arms">
       <label for="shoulders">Shoulders</label>
-      <input id="shoulders" ref="shoulders" class="mb-16" type="range" max="100">
+      <input id="shoulders" class="mb-16" type="range" max="100" v-model="fitness.shoulders">
       <label for="back">Back</label>
-      <input id="back" ref="back" class="mb-16" type="range" max="100">
+      <input id="back" class="mb-16" type="range" max="100" v-model="fitness.back">
       <label for="chest">Chest</label>
-      <input id="chest" ref="chest" class="mb-16" type="range" max="100">
+      <input id="chest" class="mb-16" type="range" max="100" v-model="fitness.chest">
       <label for="abs">Abs</label>
-      <input id="abs" ref="abs" class="mb-16" type="range" max="100">
+      <input id="abs" class="mb-16" type="range" max="100" v-model="fitness.abs">
       <label for="booty">Booty</label>
-      <input id="booty" ref="booty" class="mb-16" type="range" max="100">
+      <input id="booty" class="mb-16" type="range" max="100" v-model="fitness.booty">
       <label for="legs">Legs</label>
-      <input id="legs" ref="legs" class="mb-16" type="range" max="100">
+      <input id="legs" class="mb-16" type="range" max="100" v-model="fitness.legs">
       <button type="button" v-on:click="handleClick()">Continue</button>
     </page-no-app-bar>
   </div>`,
@@ -39,29 +55,12 @@ export default {
   },
   methods: {
     handleClick() {
-      localStorage.setItem('info_muscle_gain', this.$refs.muscle_gain.value)
-      localStorage.setItem('info_cardio', this.$refs.cardio.value)
-      localStorage.setItem('info_endurance', this.$refs.endurance.value)
-      localStorage.setItem('info_arms', this.$refs.arms.value)
-      localStorage.setItem('info_shoulders', this.$refs.shoulders.value)
-      localStorage.setItem('info_back', this.$refs.back.value)
-      localStorage.setItem('info_chest', this.$refs.chest.value)
-      localStorage.setItem('info_abs', this.$refs.abs.value)
-      localStorage.setItem('info_booty', this.$refs.booty.value)
-      localStorage.setItem('info_legs', this.$refs.legs.value)
+      localStorage.setItem('fitness', JSON.stringify(this.fitness))
       this.$router.push('/setup/finish')
     }
   },
   mounted() {
-    this.$refs.muscle_gain.value = parseInt(localStorage.getItem('info_muscle_gain'), 10) || 50
-    this.$refs.cardio.value = parseInt(localStorage.getItem('info_cardio'), 10) || 50
-    this.$refs.endurance.value = parseInt(localStorage.getItem('info_endurance'), 10) || 50
-    this.$refs.arms.value = parseInt(localStorage.getItem('info_arms'), 10) || 50
-    this.$refs.shoulders.value = parseInt(localStorage.getItem('info_shoulders'), 10) || 50
-    this.$refs.back.value = parseInt(localStorage.getItem('info_back'), 10) || 50
-    this.$refs.chest.value = parseInt(localStorage.getItem('info_chest'), 10) || 50
-    this.$refs.abs.value = parseInt(localStorage.getItem('info_abs'), 10) || 50
-    this.$refs.booty.value = parseInt(localStorage.getItem('info_booty'), 10) || 50
-    this.$refs.legs.value = parseInt(localStorage.getItem('info_legs'), 10) || 50
+    var stored = localStorage.getItem('fitness')
+    if (stored != null) this.fitness = JSON.parse(stored)
   }
 }
