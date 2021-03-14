77ef0712c6d1ca9eac803f029100aee2a1cec93d
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-03-14 14:03:47 +0100

    Update preferences.js

--- js/pages/preferences.js
+++ js/pages/preferences.js
@@ -19,6 +19,7 @@ export default {
       <li v-on:click="edit('Change Name', 'info_name', 'text')"><span><span class="material-icons-round">edit</span>Change name</span></li>
       <li v-on:click="edit('Change Age', 'info_age', 'number')"><span><span class="material-icons-round">edit</span>Change age</span></li>
       <li v-on:click="deleteData()"><span><span class="material-icons-round">delete</span>Delete data</span></li>
+      <li v-on:click="clearCache()"><span><span class="material-icons-round">delete</span>Clear cache</span></li>
     </ul>
   </page>`,
   components: {
@@ -56,6 +57,24 @@ export default {
           positiveFunction: () => {
             localStorage.clear()
             location.reload()
+          }
+        }
+      })
+      instance.$mount()
+      this.$root.$el.appendChild(instance.$el)
+    },
+    clearCache() {
+      var ComponentClass = Vue.extend(Modal)
+      var instance = new ComponentClass({
+        propsData: {
+          title: 'Clear Cache',
+          message: 'Are you sure you want to clear your cache? This cannot be undone.',
+          positiveText: 'Clear',
+          positiveFunction: () => {
+            caches.keys().then(names => {
+              for (const name of names) caches.delete(name)
+              location.reload()
+            })
           }
         }
       })
