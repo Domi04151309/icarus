90fd9ffe4229f705dedf7dbd4e3a6564028af323
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-04-27 07:55:46 +0200

    Only use blue

--- js/components/progress-sections.js
+++ js/components/progress-sections.js
@@ -10,7 +10,7 @@ export default {
   `<div class="progress">
     <div :style="'z-index: 4;width: calc(' + (s1 * 100 / 4) + '% + 12px)'"></div>
     <div class="light-blue" :style="'z-index: 3;width: calc(' + (s2 * 100 / 4) + '% + 12px)'"></div>
-    <div class="green" :style="'z-index: 2;width: calc(' + (s3 * 100 / 4) + '% + 12px)'"></div>
-    <div class="red" :style="'z-index: 1;width: calc(' + (s4 * 100 / 4) + '% + 12px)'"></div>
+    <div class="cyan" :style="'z-index: 2;width: calc(' + (s3 * 100 / 4) + '% + 12px)'"></div>
+    <div class="teal" :style="'z-index: 1;width: calc(' + (s4 * 100 / 4) + '% + 12px)'"></div>
   </div>`
 }
