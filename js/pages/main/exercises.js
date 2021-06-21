b92504f0b7588e24f632c317b4a24df769804bc0
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-06-21 14:10:53 +0200

    Redesign exercise page

--- js/pages/main/exercises.js
+++ js/pages/main/exercises.js
@@ -6,17 +6,42 @@ import Modal from '../../components/modal.js'
 
 import ExercisesHelper from '../../helpers/exercises.js'
 
+//TODO: Full workouts
+
 export default {
   name: 'exercises',
   data() {
     return {
-      recommended: ExercisesHelper.getRecommended()
+      recommended: ExercisesHelper.getRecommended(),
+      combinations: ['Full Body', 'Abs', 'Legs & Booty', 'Upper Body']
     }
   },
   template:
   `<page-tab-bar class="red">
     <h2 class="mt-16 mx-8 secondary-title">Exercises</h2>
     <p class="mt-0 mb-48 mx-8">An exercise a day keeps the doctor away!</p>
+    <router-link to="/exercises/exercise-list" class="card mb-16-p-16 flex center">
+      <div class="material-icons-round big-c-icon">directions_run</div>
+      <div>
+        <h2 class="m-0 mt-2">Without Equipment</h2>
+        <p>Things you can do right away</p>
+      </div>
+    </router-link>
+    <router-link to="/exercises/exercise-list" class="card mb-16-p-16 flex center">
+      <div class="material-icons-round big-c-icon">fitness_center</div>
+      <div>
+        <h2 class="m-0 mt-2">With Equipment</h2>
+        <p>Things you need equipment for</p>
+      </div>
+    </router-link>
+    <router-link to="/exercises/exercise-list" class="card mb-16-p-16 flex center">
+      <div class="material-icons-round big-c-icon">self_improvement</div>
+      <div>
+        <h2 class="m-0 mt-2">Yoga</h2>
+        <p>Find inner balance</p>
+      </div>
+    </router-link>
+    <h2 class="mx-8 mt-48 mb-24">Perfect Fits For You</h2>
     <div class="grid-2 gap-16 mb-16">
       <exercise-item
         v-for="item in recommended"
@@ -26,9 +51,20 @@ export default {
         :image="item.image">
       </exercise-item>
     </div>
-    <router-link to="/exercises/exercise-list" class="card p-16 text-center">
-      All Exercises
+    <h2 class="mx-8 mt-48 mb-24">Full Workouts</h2>
+
+    <div class="disabled">
+
+    <router-link v-for="item in combinations" :key="item" to="" class="card mb-16-p-16 flex center">
+      <div class="material-icons-round big-c-icon">inventory</div>
+      <div>
+        <h2 class="m-0 mt-2">{{ item }} Workout</h2>
+        <p>Train your {{ item.toLowerCase() }}</p>
+      </div>
     </router-link>
+
+    </div>
+
   </page-tab-bar>`,
   components: {
       PageTabBar,
