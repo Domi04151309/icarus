cfb8966a9aedb967e20a44944394cc279366b50f
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-07-01 20:17:33 +0200

    Update image-grabber.js

--- tools/image-grabber.js
+++ tools/image-grabber.js
@@ -16,10 +16,12 @@ export default {
     <h2>Controls</h2>
     <button type="button" v-on:click="$router.go()">Refresh</button>
     <h2>Nutrition</h2>
-    <div class="grid-1-3 gap-16 mb-16">
-      <div v-for="(item, i) in items":key="i" class="card exercise img-card">
-        <img :src="item.image" alt="preview image" loading="lazy">
-        <h2 class="m-0 p-16">{{ item.title }}</h2>
+    <div v-for="(item, i) in items" :key="i">
+      <h3>{{ item.title }}</h3>
+      <div class="grid-3 gap-16">
+        <div v-for="image in item.images" :key="image" class="card exercise img-card">
+          <img :src="image" alt="Image" loading="lazy">
+        </div>
       </div>
     </div>
   </page>`,
@@ -35,7 +37,14 @@ export default {
     array.forEach(item => {
       items.push({
         title: item.title,
-        image: 'https://source.unsplash.com/featured/?food,' + item.title
+        images: [
+          'https://source.unsplash.com/featured/?food,' + item.title,
+          'https://source.unsplash.com/featured/?drink,' + item.title,
+          'https://source.unsplash.com/featured/?' + item.title,
+          'https://source.unsplash.com/1920x1080/?food,' + item.title,
+          'https://source.unsplash.com/1920x1080/?drink,' + item.title,
+          'https://source.unsplash.com/1920x1080/?' + item.title
+        ]
       })
     })
 
